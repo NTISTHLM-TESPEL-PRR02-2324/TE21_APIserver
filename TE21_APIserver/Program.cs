70caa8f2cca6193abaa577049c14d0f8c5bc876d
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2024-02-02 12:34:24 +0100

    sryj

--- TE21_APIserver/Program.cs
+++ TE21_APIserver/Program.cs
@@ -3,9 +3,15 @@ using TE21_APIserver;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
+TeacherData data = new();
+
 app.MapGet("/", GetSomething);
 app.MapGet("/hello", () => "Goodbye!");
-app.MapGet("/teachers/{number}", GetTeacher);
+app.MapGet("/teachers/{number}", data.GetTeacher);
+
+app.MapPost("/teachers", data.PostTeacher);
+
+app.Urls.Add("http://*:5281");
 
 app.Run();
 
@@ -14,12 +20,26 @@ static string GetSomething()
   return "Hello TE21A";
 }
 
-static Teacher GetTeacher(int number)
+class TeacherData
 {
   List<Teacher> teachers = new() {
     new Teacher() {Name = "Micke", HitPoints = 100},
-    new Teacher() {Name = "Martin", HitPoints = 3}
+    new Teacher() {Name = "Martin", HitPoints = 3},
+    new Teacher() {Name = "Lena", HitPoints = 9000}
   };
 
-  return teachers[number];
+  public IResult PostTeacher()
+  {
+    Console.WriteLine("Yay");
+    return Results.Ok();
+  }
+
+  public IResult GetTeacher(int number)
+  {
+    if (number < 0 || number >= teachers.Count)
+    {
+      return Results.NotFound();
+    }
+    return Results.Ok(teachers[number]);
+  }
 }
